d3c5c7a5f23a0178f3784b9ad9b6b04b34c822af
Tero Paavolainen <Tero Paavolainen>
2017-01-21 00:43:05 +0200

    menu.......

---
+++ SnakariDM/Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    [SerializeField]
+    Image bg;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainFight");
+    }
+
+
+    public void EMT()
+    {
+        bg.color = Random.ColorHSV();
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
