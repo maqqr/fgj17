b40298b60f0b47cc3875be7b046442dbbeaa40c2
Tero Paavolainen <Tero Paavolainen>
2017-01-21 22:57:06 +0200

    musthave json chaos included

---
+++ SnakariDM/Assets/DataFile.cs
@@ -0,0 +1,28 @@
+﻿public class DataFile
+{
+    public string content;
+    public string name;
+    public string extension;
+
+    public DataFile()
+    {
+    }
+
+    public DataFile(string name, string content)
+    {
+        this.content = content;
+        this.name = name;
+    }
+
+    public DataFile(string name, string content, string extension)
+    {
+        this.name = name;
+        this.content = content;
+        this.extension = extension;
+    }
+
+    public DataFile(string content)
+    {
+        this.content = content;
+    }
+}
